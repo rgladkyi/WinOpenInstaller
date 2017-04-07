3ffb3be0f781da5a5513e188bb95046c5276c409
rgladkyi <roman.gladkuy@gmail.com>
2017-04-07 17:14:01 +0300

    Added/Updated ditto_install.bat.

--- InstallScripts/ditto_install.bat
+++ InstallScripts/ditto_install.bat
@@ -2,10 +2,10 @@
 
 set DL_FOLDER=%1
 set DL_FOLDER_PR1=%DL_FOLDER:"=%
-set DL_FILE_PATH=%DL_FOLDER_PR1%\Notepad++.zip
+set DL_FILE_PATH=%DL_FOLDER_PR1%\Ditto.zip
 
 echo %DL_FILE_PATH%
 
-call C:\CMDEX\WinOpenInstaller\InstallScripts\download_and_unzip.bat "https://rgladkyi.github.io/CMDEX/Tools/Ditto.zip" %DL_FILE_PATH%
+call C:\CMDEX\WinOpenInstaller\InstallScripts\download_and_unzip.bat "https://rgladkyi.github.io/WinOpenInstaller/raw/master/Tools/Ditto.zip" %DL_FOLDER% %DL_FILE_PATH%
 
 
