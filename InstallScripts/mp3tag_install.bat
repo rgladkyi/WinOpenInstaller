ba5bca7ab8362da24f48b5c4570c4b744c94b429
rgladkyi <roman.gladkuy@gmail.com>
2017-05-03 01:48:03 +0300

    Added/Updated mp3tag_install.bat.

---
+++ InstallScripts/mp3tag_install.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set DL_FOLDER=%1
+set DL_FOLDER_PR1=%DL_FOLDER:"=%
+set DL_FILE_PATH=%DL_FOLDER_PR1%\Mp3tag.zip
+
+echo %DL_FILE_PATH%
+
+call C:\CMDEX\WinOpenInstaller\InstallScripts\download_and_unzip.bat "https://github.com/rgladkyi/WinOpenInstaller/raw/master/Tools/Mp3tag.zip" %DL_FOLDER% %DL_FILE_PATH%
+
+
