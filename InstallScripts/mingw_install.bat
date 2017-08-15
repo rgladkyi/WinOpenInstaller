36595c7248b5441a594fda0fc8f85fa8388c3e9d
rgladkyi <roman.gladkuy@gmail.com>
2017-08-15 18:57:13 +0300

    Added/Updated *.

--- InstallScripts/mingw_install.bat
+++ InstallScripts/mingw_install.bat
@@ -1,11 +1,11 @@
 @echo off
 
 set DL_FOLDER=%1
-set DL_FOLDER_PR1=%DL_FOLDER%
-set DL_FILE_PATH=%DL_FOLDER_PR1%\Notepad++.zip
+set DL_FOLDER_PR1=%DL_FOLDER:"=%
+set DL_FILE_PATH="%DL_FOLDER_PR1%\MinGW.zip"
 
 echo %DL_FILE_PATH%
 
-call "%CMDEX_ROOT_DIR%\WinOpenInstaller\InstallScripts\download_and_unzip.bat" "https://github.com/rgladkyi/WinOpenInstaller/raw/master/Tools/MinGW.zip" %DL_FILE_PATH%
+call "%CMDEX_ROOT_DIR%\WinOpenInstaller\InstallScripts\download_and_unzip.bat" "https://github.com/rgladkyi/WinOpenInstaller/raw/master/Tools/MinGW.zip" %DL_FOLDER% %DL_FILE_PATH%
 
 
